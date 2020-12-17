390399440223c3060ee268a74e16b2adc53001dc
Zeikov <76076857+Zeikov@users.noreply.github.com>
2020-12-17 12:18:50 -0600

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -5,50 +5,7 @@
  */
 
 
-var opt_1 = new Array("-", "Chai Verde", "Gun Powder Menta", "Choco Vainilla", "Jasmine Mint", "Té verde de Indonesia");
-			var opt_2 = new Array("-","Apple Chai","Vainilla-Jazmín","Pai Mu Tan");
-			var opt_3 = new Array("-", "English Breakfast tea","Earl Grey con Bergamota","Earl Grey Creme","Té negro con chispas de chocolate","Malasa chai");
-            var opt_4 = new Array("-","Frutos Rojos Silvestres","Kane-wa","Granada-jenjibre","Mandarina-Jengibre","Flores de campo","Manzana-vainilla");
 
-
-            // 2) crear una funcion que permita ejecutar el cambio dinamico
-
-			function cambia() {
-				var cosa;
-				//Se toma el vamor de la "cosa seleccionada"
-				cosa = document.getElementById('cosa').value;
-
-				//se chequea si la "cosa" esta definida
-				if (cosa != 0) {
-					//selecionamos las cosas Correctas
-					mis_opts = eval("opt_" + cosa);
-					document.getElementById("opi").innerHTML = mis_opts[i];
-					//se calcula el numero de cosas
-					num_opts = mis_opts.length;
-					//marco el numero de opt en el select
-					cosa.setAttribute("option", "mis_opts")
-					//para cada opt del array, la pongo en el select
-					for (i = 0; i < num_opts; i++) {
-						document.opt.options[i].value = mis_opts[i];
-						document.opt.options[i].text = mis_opts[i];
-					}
-				} else {
-					//si no habia ninguna opt seleccionada, elimino las cosas del select
-					document.length = 1;
-					//ponemos un guion en la unica opt que he dejado
-					document.opt.options[0].value = "-";
-					document.opt.options[0].text = "-";
-				}
-				//hacer un reset de las opts
-				document.opt.options[0].selected = true;
-
-			}
-			const selectElement = document.querySelector('.nieve');
-
-selectElement.addEventListener('change', (event) => {
-    const resultado = document.querySelector('.resultado');
-    resultado.textContent = `Te gusta el sabor ${event.target.value}`;
-});
 function getType() {
   var x = document.getElementById("cosa").value;
   var items;
