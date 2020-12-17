272beaba90bb0543bec223606d80589112d2caa4
Zeikov <76076857+Zeikov@users.noreply.github.com>
2020-12-17 12:09:54 -0600

    Update productos.js

--- js/productos.js
+++ js/productos.js
@@ -16,31 +16,64 @@ var opt_1 = new Array("-", "Chai Verde", "Gun Powder Menta", "Choco Vainilla", "
 			function cambia() {
 				var cosa;
 				//Se toma el vamor de la "cosa seleccionada"
-				cosa = document.formulario1.cosa[document.formulario1.cosa.selectedIndex].value;
+				cosa = document.getElementById('cosa').value;
+
 				//se chequea si la "cosa" esta definida
 				if (cosa != 0) {
 					//selecionamos las cosas Correctas
 					mis_opts = eval("opt_" + cosa);
+					document.getElementById("opi").innerHTML = mis_opts[i];
 					//se calcula el numero de cosas
 					num_opts = mis_opts.length;
 					//marco el numero de opt en el select
-					document.formulario1.opt.length = num_opts;
+					cosa.setAttribute("option", "mis_opts")
 					//para cada opt del array, la pongo en el select
 					for (i = 0; i < num_opts; i++) {
-						document.formulario1.opt.options[i].value = mis_opts[i];
-						document.formulario1.opt.options[i].text = mis_opts[i];
+						document.opt.options[i].value = mis_opts[i];
+						document.opt.options[i].text = mis_opts[i];
 					}
 				} else {
 					//si no habia ninguna opt seleccionada, elimino las cosas del select
-					document.formulario1.opt.length = 1;
+					document.length = 1;
 					//ponemos un guion en la unica opt que he dejado
-					document.formulario1.opt.options[0].value = "-";
-					document.formulario1.opt.options[0].text = "-";
+					document.opt.options[0].value = "-";
+					document.opt.options[0].text = "-";
 				}
 				//hacer un reset de las opts
-				document.formulario1.opt.options[0].selected = true;
+				document.opt.options[0].selected = true;
 
 			}
+			const selectElement = document.querySelector('.nieve');
+
+selectElement.addEventListener('change', (event) => {
+    const resultado = document.querySelector('.resultado');
+    resultado.textContent = `Te gusta el sabor ${event.target.value}`;
+});
+function getType() {
+  var x = document.getElementById("cosa").value;
+  var items;
+  if (x === "verde") {
+    items = ["-", "Chai Verde", "Gun Powder Menta", "Choco Vainilla", "Jasmine Mint", "Té verde de Indonesia"];
+  } if(x=== "blanco")  {
+    items = ["-","Apple Chai","Vainilla-Jazmín","Pai Mu Tan"]
+  }
+	if(x=== "negro")  {
+    items = ["-", "English Breakfast tea","Earl Grey con Bergamota","Earl Grey Creme","Té negro con chispas de chocolate","Malasa chai"]
+  }
+	if(x=== "tisana")  {
+    items = ["-","Frutos Rojos Silvestres","Kane-wa","Granada-jenjibre","Mandarina-Jengibre","Flores de campo","Manzana-vainilla"]
+  }
+  var str = ""
+	var stv = ""
+  for (var item of items) {
+		stv += "value" + item 
+
+		str += "<option>" + item + "</option>"
+
+  }
+  document.getElementById("opi").innerHTML = str;
+}
+
 //Funcion para ocultar el contenido de los té
 function ocultarv() {
     var v = document.getElementById("verde");
